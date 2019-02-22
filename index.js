493334fb821a6bfe5eb3cde354e4f2c4c2b0c85b
tlamont06 La <tlamont06@gmail.com>
2019-02-22 14:00:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-katzDeliLine = [];
-
-function takeANumber(currentLine, name){
-  var array = [];
-  var i = 1;
-  for (let i = 1; i < array.length; i++){
- return array.push(`Welcome, ${name}. You are number ${i} in line.`);
+ katzDeliLine = [];
+ 
+ function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+ }
+ 
+ 
+ 
+ function nowServing(katzDeliLine){
+  // returns an empty line when no one is there
+  // returns currently serving announcement and shifts line
+  
+ }
+ 
+ function currentLine(katzDeliLine){
+  // returns String
+  // says who is in line when there are people
+ }
+ 
+ /*function iLoveTheBeatles (num){
+  var i = 0;
+  let array = [];
+  if(num === 17){
+    array.push('I love the Beatles!') 
+    }else{
+  do{array.push('I love the Beatles!')
+  i++;
+} while(i <= num);
 }
 return array;
 }
+*/
